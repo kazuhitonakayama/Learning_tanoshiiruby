8e4621c42ca0e5272951f4b2d4ac2b4003db9a95
kazuhitonakayama <firstjin19990101@gmail.com>
2021-05-09 22:47:07 +0900

    "と'の違い

---
+++ chapter1/hello_world.rb
@@ -0,0 +1,7 @@
+print("Hello, \nRuby\n!\n") # output => Hello,
+                            # Ruby
+                            # !
+print('Hello, \nRuby\n!\n') # output => Hello, \nRuby\n!\n%  
+
+# "はその内容の特殊文字を認識するが、'は特殊文字をそのまま表示する
+# ただし、\と'を表示することは'の内部においても可能であり、\\また、\を付与することで表示できる
