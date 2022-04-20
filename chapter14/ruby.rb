0a987d31d66becf59937ebd2afc62e0565bbad68
kazuhitonakayama <firstjin19990101@gmail.com>
2022-04-20 22:41:49 +0900

    `slice`は非破壊なメソッドで、`slice`は破壊的なメソッド

--- chapter14/ruby.rb
+++ chapter14/ruby.rb
@@ -21,3 +21,13 @@ hello = hello.concat(world)
 p "\"#{hello}\"と\"#{world}\"をつなげるよ"
 p "連結の結果：#{hello}"
 p "元々の文字列は：#{original}"
+
+p "========="
+str = "abcde"
+str.slice(1, 3)
+p "元の文字列：#{str}：sliceは非破壊"
+
+p "========="
+str = "abcde"
+str.slice!(1, 3)
+p "元の文字列：#{str}：slice!は破壊的"
