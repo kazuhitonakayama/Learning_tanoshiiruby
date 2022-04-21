ec957413d9718de82da5c560892fbf3b08be684d
kazuhitonakayama <firstjin19990101@gmail.com>
2022-04-21 23:15:32 +0900

    chapter14の第4問別解

--- chapter14/exercise.rb
+++ chapter14/exercise.rb
@@ -6,11 +6,13 @@ p list
 
 p "=========="
 p "問題2：1の配列をアルファベット順に並び替えよう"
-list.each do |item|
-  item.downcase!
-end
 
 p list.sort_by {|l| -l}
+p "答えは"
+p "=========="
+p "問題3：大小の区別をせずに、アルファベット順に並び替えよう"
+
+p list.sort_by {|l| l.downcase}
 
 p "=========="
 p "問4：数を調べ上げよう"
@@ -28,4 +30,19 @@ sorted_list.each do |word|
     used_word_list << word
     print "\n'#{word}\'：*"
   end
+end
+
+p ""
+p "別解"
+
+str = "Ruby is an object oriented programming language"
+hash = Hash.new
+
+str.each_char do |char|
+  hash[char] = 0 if not hash[char]
+  hash[char] += 1
+end
+
+hash.keys.sort.each do |k|
+  printf("'%s'： %s\n", k, "*" * hash[k])
 end
