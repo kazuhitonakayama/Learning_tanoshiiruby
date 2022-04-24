e7e6f051b395a05a24c5b312bf20688fa5d40208
kazuhitonakayama <firstjin19990101@gmail.com>
2022-04-24 10:03:17 +0900

    chapter15：Hash 練習問題第二問：ハッシュのメソッドを使ってペアの数を数えてください

--- chapter15/exercise.rb
+++ chapter15/exercise.rb
@@ -10,3 +10,18 @@ wday.store(:friday, "金曜日")
 wday.store(:saturday, "土曜日")
 
 p wday[:sunday]
+
+p "=================="
+
+p "練習問題第二問：ハッシュのメソッドを使ってペアの数を数えてください"
+wday = Hash.new
+
+wday.store(:sunday, "日曜日")
+wday.store(:monday, "月曜日")
+wday.store(:tuesday, "火曜日")
+wday.store(:wednesday, "水曜日")
+wday.store(:thursday, "木曜日")
+wday.store(:friday, "金曜日")
+wday.store(:saturday, "土曜日")
+
+p wday.size
