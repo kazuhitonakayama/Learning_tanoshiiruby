ee95171cdb5552fd105f388ce424a0d95096a22c
kazuhitonakayama <firstjin19990101@gmail.com>
2022-04-24 18:36:36 +0900

    chapter15の練習問題の第三、第四問を解決

--- chapter15/exercise.rb
+++ chapter15/exercise.rb
@@ -25,3 +25,28 @@ wday.store(:friday, "金曜日")
 wday.store(:saturday, "土曜日")
 
 p wday.size
+
+
+p "=================="
+
+p "練習問題第三問：「sunday」は日曜日のことです、のような文字列を出力させてください"
+
+wday.each do |key, value|
+  p "「#{key}」は#{value}のことです"
+end
+
+p "=================="
+
+p "練習問題第四問：str2hashメソッドを定義しなさい"
+def str2hash(given_str)
+  result_hash = Hash.new
+  list = given_str.split(/\s+/)
+  while key = list.shift
+    value = list.shift
+    result_hash[key] = value
+  end
+  return result_hash
+end
+
+result = str2hash("blue 青 white 白 \nred 赤")
+p result
