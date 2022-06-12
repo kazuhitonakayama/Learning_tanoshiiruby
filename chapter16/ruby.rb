825d85dc199d4f97b20831979ac5a5fab1d02f1e
kazuhitonakayama <firstjin19990101@gmail.com>
2022-06-12 22:04:23 +0900

    `%r()`などでもRegexオブジェクトを作成できる

--- chapter16/ruby.rb
+++ chapter16/ruby.rb
@@ -4,6 +4,18 @@ re = Regexp.new("Ruby")
 p re
 
 p "============="
+p "%r、でも作成できるよ"
+re = %r(Ruby)
+p re
+
+p "============="
 p "正規表現 =~ 文字列、で正規表現比較ができる"
 re = /A.C/
-p re =~ "VBC"
+p re =~ "VBC" 
+
+options = "options"
+hash = {
+  options => "中山"
+}
+
+p hash
